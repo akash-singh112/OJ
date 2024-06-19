cb8843dc45758a4a434bf69dde8e9d88020dfd4a
Akash Singh <akash_s@me.iitr.ac.in>
2024-06-19 11:34:17 +0530

    login auth

--- online judge/backend/index.js
+++ online judge/backend/index.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const User = require('./database/user.js');
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 dotenv.config();
 
 const port = 8080;
@@ -12,6 +13,7 @@ const app = express();
 //add middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cookieParser())
 
 app.get('/home',(req,res)=>{
     res.send('Welcome to Home Page\n')
@@ -53,19 +55,57 @@ app.post('/register',async (req,res)=>{
         const tkn = jwt.sign({id:user1._id,email},process.env.SECRET_KEY,{
             expiresIn:'1h'
         });
-        user1.tkn = user1;
+        user1.tkn = tkn;
         //for security reasons, also we dont need password anymore so discarding it
         user1.password = undefined;
         //200 => successful
         res.status(200).json({message : 'Registration successful' , user1});
     } catch (error) {
-        console.log(error);
+        console.log(error.message);
     }
 
 })
 
 app.post('/login',async (req,res)=>{
+    try {
+        //get user details
+        const {email,password} = req.body;
 
+        //check if both email and password are filled up
+        if(email==undefined || password==undefined){
+            return res.status(400).send('Please enter all fields');
+        }
+
+        //check if user exists in DB
+        const user = await User.findOne({email});
+        if(user==undefined){
+            return res.status(400).send('This user does not exist');
+        }
+
+        //check for correctpassword
+        const correct = await bcrypt.compare(password,user.password);
+        if(!correct){
+            return res.status(400).send('Incorrect password');
+        }
+
+        //generate jwt token for secure access
+        const tkn = jwt.sign({id:user._id},process.env.SECRET_KEY,{
+            expiresIn:'1h'
+        });
+        user.tkn = tkn;
+        user.password = undefined;
+
+        //store cookies and send successful login message
+        res.status(200).cookie('cookie1',tkn,{ maxAge : 360000 , httpOnly:true }).json({
+            message:'Login successful',
+            success:true,
+            tkn
+        });
+
+    } catch (error) {
+        //console.log('galat hai aap');
+        console.log(error.message);
+    }
 })
 
 app.get('/:universalURL',(req,res)=>{
