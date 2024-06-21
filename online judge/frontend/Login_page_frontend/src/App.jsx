828e4260cfd921d919f86992206da4da06534909
Akash Singh <akash_s@me.iitr.ac.in>
2024-06-21 16:03:03 +0530

    linked login FE/BE

--- online judge/frontend/Login_page_frontend/src/App.jsx
+++ online judge/frontend/Login_page_frontend/src/App.jsx
@@ -1,31 +1,55 @@
-import { useState ,useEffect , useRef} from 'react'
+import { useRef, useState } from 'react'
 import boxicons from 'boxicons'
+import {sendDataFromFrontendToBackend} from '../api_link/api_link_login.js'
 import './App.css'
 
-//frontend_port for login//
+//frontend_port 2069 for login//
+//backend port 8800 for login//
 const port = 2069;
 
 function App() {
-  const ip_ref = useRef();
+  const [details,setDetails] = useState({
+    email:'',
+    password:'',
+  });
+  const [result,setResult] = useState('');
 
-  const focus1 = () =>{
-    ip_ref.current.focus();
+  const handleChange = (e)=>{
+    const {name,value} = e.target;
+    setDetails(prev=>{
+      return {...prev,[name]:value};
+    })
+    console.log('yahan hu1');
+    console.log(details);
   }
+
+  const handleSubmit = async (e)=>{
+    //to prevent default behaviour of refreshing when form is submitted
+    e.preventDefault();
+    //console.log(details);
+
+    const response = await sendDataFromFrontendToBackend(details);
+    setResult(response.message);
+  }
+
   return (
     <div className="container">
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <h1>Login</h1>
         <div className="ip_box">
-            <input ref={ip_ref} type="text" placeholder='Email-ID' required:true/>
-            <i class='bx bxs-user'></i>
+            <input type="email" name='email' placeholder='Email-ID' required onChange={handleChange}/>
+            <i className='bx bxs-user'></i>
         </div>
         <div className="ip_box">
-            <input type="password" placeholder='Password' required:true/>
-            <i class='bx bx-lock'></i>
+            <input type="password" name='password' placeholder='Password' required onChange={handleChange}/>
+            <i className='bx bx-lock'></i>
         </div>
-        <button type='submit' className='button1' onClick={focus1}>Login</button>
+        <button type='submit' className='button1'>Login</button>
         <div className="register_link">
-          <p>Don't have an account?<a href="#">Register</a></p>
+          <p>Don't have an account?<a href="http://localhost:2070/register">Register</a></p>
+        </div>
+        <div className="response">
+          <p>{result}!</p>
         </div>
       </form>
     </div>
