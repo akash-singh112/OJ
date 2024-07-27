488051d5efbd0e3182f4ede0c21201cb1e33f559
Akash Singh <akash_s@me.iitr.ac.in>
2024-07-27 22:48:40 +0530

    optimizations

--- online_judge/frontend/src/private.jsx
+++ online_judge/frontend/src/private.jsx
@@ -2,5 +2,5 @@ import { Navigate } from "react-router-dom";
 
 export const PrivateRoute = ({children})=>{
     const success = localStorage.getItem('login');
-    return success ? children : <Navigate to='/home/login' />;
+    return success ? children : <Navigate to='/login' />;
 }
